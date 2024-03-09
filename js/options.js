e5f2a69d5bd684d8017f32c3fd339c782ef3ef8e
Richard Bale <rich@icuknet.co.uk>
2024-03-09 17:16:11 +0000

    Fix various bugs and get things working correctly

--- js/options.js
+++ js/options.js
@@ -21,7 +21,7 @@ const collectionsInterface = {
     fetch('https://api.raindrop.io/rest/v1/collections', {
         method: 'GET',
         headers: {
-          'Authorization': 'Bearer ' + accessToken
+          'Authorization': 'Bearer ' + tokenInterface.accessTokenInput.value
         }
       })
       .then(response => {
@@ -142,7 +142,143 @@ const utils = {
   
 }
 
+const tokenInterface = {
+  accessTokenInput: document.getElementById('accessToken'),
+  saveTokenButton: document.getElementById('saveTokenButton')
+}
 
+const syncInterface = {
+  syncFoldersButton: document.getElementById('syncFolders'),
+  syncStatus: document.getElementById('task-status'),
+  currentAction: document.getElementById('currentAction'),
+
+  populateBookmarks: async (items) => {
+    this.currentAction.textContent = 'Create New Folders';
+    await bookmarkUtils.createBookmarkFolders(folderInterface.chosenFolder.toString(), items);
+    this.currentAction.textContent = 'Folders created :)';
+
+    const flattenedItems = utils.flattenItems(items);
+
+   // const requestsPerMinute = 120;
+   // const rateLimit = 60 * 1000 / requestsPerMinute; // Convert minutes to milliseconds
+    const maxRequestsPerInterval = 115; // Set to 5 less than the limit
+    let currentIndex = 0;
+    let requestCounter = 0;
+
+    async function fetchDataForPage(item, page = 0) {
+      // Use the bookmarkId stored in the item
+      const parentId = item.bookmarkId;
+      const perPage = 50;
+      this.currentAction.textContent = `Fetch bookmarks from Raindrop ${item.id}?page=${page}&perpage=${perPage}`;
+      const response = await fetch(`https://api.raindrop.io/rest/v1/raindrops/${item.id}?page=${page}&perpage=${perPage}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + tokenInterface.accessTokenInput.value
+        }
+      });
+  
+      const data = await response.json();
+  
+      // Process additional data
+      for (const item of data.items) {
+        await bookmarkUtils.createBookmark(String(parentId), item.title, item.link);
+      }
+      // Increment the request counter
+      requestCounter++;
+  
+      // Check if the limit is reached
+      if (requestCounter >= maxRequestsPerInterval) {
+        // Reset the counter and wait for the remaining time in the rate limit interval
+        requestCounter = 0;
+        //Wait for 1 minute before carrying on
+        this.currentAction.textContent = 'Wait for 1 minute';
+        await new Promise(resolve => setTimeout(resolve, 60000));
+      }
+
+      // Check if there are more pages to fetch, page starts at 0 which messes up this calculation
+      let tempPage = page+1; //Page is zero indexed, need to +1 to get proper checks
+      const totalItemsProcessed = tempPage * perPage;
+      console.log('Total items processed: ', totalItemsProcessed);
+      console.log('Data Count: ', data.count);
+      if (totalItemsProcessed < data.count) {
+        // Move to the next page
+        await fetchDataForPage(item, page + 1);
+      } else {
+        // Update progress and move to the next item
+        utils.updateProgress(currentIndex + 1, flattenedItems.length);
+        currentIndex++;
+        if (currentIndex < flattenedItems.length) {
+          fetchDataForPage(flattenedItems[currentIndex]);
+        }
+      }
+    }
+  
+    // Start processing items
+    if (currentIndex < flattenedItems.length) {
+      await fetchDataForPage(flattenedItems[currentIndex]);
+      //TODO - There is a bug here, this code runs earlier in the process than it should. Like it isn't waiting for fetchDataForPage to complete.
+      //syncInterface.currentAction.textContent = 'All Done';
+      //syncInterface.syncStatus.classList.add('hide');
+    }
+
+  },
+
+  grabBookmarksAndPopulate: async function () {
+    //Unhide the status bits
+    this.syncStatus.classList.remove('hide');
+    this.currentAction.textContent = 'Remove Folders';
+
+    // Remove all bookmarks in folder ID '1' (bookmarks bar)
+    await bookmarkUtils.removeBookmarksInFolder('1');
+
+    let childCopy = [], childCollections = [], selectedItems = [], parentMap = {};
+
+    // Here we are going to loop through the list of children, looking for everything that is in one particular collection
+    fetch('https://api.raindrop.io/rest/v1/collections/childrens', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + tokenInterface.accessTokenInput.value
+      }
+    })
+    .then(async (response) => {
+      const data = await response.json();
+
+      childCollections = data.items;
+
+      function findChildren(parentId, items) {
+        const children = items.filter(item => item.parent && item.parent.$id === parentId);
+
+        // Create an array to store the children of the current parent
+        const childrenArray = [];
+
+        // Recursively call the function for each found child
+        children.forEach(child => {
+          let tmp = {
+            id: child._id,
+            title: child.title,
+            children: []
+          };
+
+          // Recursively find and add children to the current child
+          tmp.children = findChildren(tmp.id, items);
+
+          // Add the current child to the array
+          childrenArray.push(tmp);
+
+        });
+
+        return childrenArray;
+      }
+
+      // console.log(collectionsInterface.chosenCollection);
+
+      selectedItems = findChildren(collectionsInterface.chosenCollection, childCollections);
+
+      syncInterface.populateBookmarks(selectedItems);
+
+    }); // End then
+  }
+}
 
 
 
@@ -152,17 +288,10 @@ const utils = {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    const accessTokenInput = document.getElementById('accessToken');
-    const saveTokenButton = document.getElementById('saveTokenButton');
-    
-    const syncFolders = document.getElementById('syncFolders');
-    
 
-
-  
     // Save access token
-    saveTokenButton.addEventListener('click', function() {
-      const accessToken = accessTokenInput.value;
+    tokenInterface.saveTokenButton.addEventListener('click', function() {
+      const accessToken = tokenInterface.accessTokenInput.value;
       chrome.storage.sync.set({ 'accessToken': accessToken }, function() {
         console.log('Access token saved:', accessToken);
         collectionsInterface.fetchCollections(accessToken);
@@ -172,7 +301,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Load saved access token and selectedCollection
     chrome.storage.sync.get(['accessToken', 'selectedCollection','selectedFolder'], function(result) {
       if (result.accessToken) {
-        accessTokenInput.value = result.accessToken;
+        tokenInterface.accessTokenInput.value = result.accessToken;
       
         // Load and populate collections only if an access token is saved
         if (result.selectedCollection) {
@@ -195,7 +324,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
     collectionsInterface.fetchCollectionButton.addEventListener('click', function() {
       // Call the fetchCollections method with the saved access token
-      collectionsInterface.fetchCollections(accessTokenInput.value);
+      collectionsInterface.fetchCollections(tokenInterface.accessTokenInput.value);
     });
   
     // Save selected collection
@@ -213,129 +342,13 @@ document.addEventListener('DOMContentLoaded', function() {
       folderInterface.saveSelectedFolder();
     });
 
-
-    async function populateBookmarks(items) {
-      await bookmarkUtils.createBookmarkFolders(folderInterface.chosenFolder.toString(), items);
-
-      const flattenedItems = utils.flattenItems(items);
-
-     // const requestsPerMinute = 120;
-     // const rateLimit = 60 * 1000 / requestsPerMinute; // Convert minutes to milliseconds
-      const maxRequestsPerInterval = 115; // Set to 5 less than the limit
-      let currentIndex = 0;
-      let requestCounter = 0;
-
-      async function fetchDataForPage(item, page = 0) {
-        // Use the bookmarkId stored in the item
-        const parentId = item.bookmarkId;
-        const perPage = 50;
-    
-        const response = await fetch(`https://api.raindrop.io/rest/v1/raindrops/${item.id}?page=${page}&perPage=${perPage}`, {
-          method: 'GET',
-          headers: {
-            'Authorization': 'Bearer ' + accessTokenInput.value
-          }
-        });
-    
-        const data = await response.json();
-    
-        // Process additional data
-        for (const item of data.items) {
-          await bookmarkUtils.createBookmark(String(parentId), item.title, item.link);
-        }
-        // Increment the request counter
-        requestCounter++;
-    
-        // Check if the limit is reached
-        if (requestCounter >= maxRequestsPerInterval) {
-          // Reset the counter and wait for the remaining time in the rate limit interval
-          requestCounter = 0;
-          //Wait for 1 minute before carrying on
-          await new Promise(resolve => setTimeout(resolve, 60000));
-        }
-
-        // Check if there are more pages to fetch
-        const totalItemsProcessed = page * perPage;
-        if (totalItemsProcessed < data.count) {
-          // Move to the next page
-          await fetchDataForPage(item, page + 1);
-        } else {
-          // Update progress and move to the next item
-          utils.updateProgress(currentIndex + 1, flattenedItems.length);
-          currentIndex++;
-          if (currentIndex < flattenedItems.length) {
-            fetchDataForPage(flattenedItems[currentIndex]);
-          }
-        }
-      }
-    
-      // Start processing items
-      if (currentIndex < flattenedItems.length) {
-        fetchDataForPage(flattenedItems[currentIndex]);
-      }
-    }
-
-
-
   
     // Save selected collection
     syncFolders.addEventListener('click', async function() {
-     
-        //Remove all bookmarks in folder ID '1' (bookmarks bar)
-        await bookmarkUtils.removeBookmarksInFolder('1');
-
-        let childCopy = [], childCollections = [], selectedItems = [], parentMap = {};
-
-    //Here we are going to loop through the list of children, looking for everything that is in one particular collection
-    fetch('https://api.raindrop.io/rest/v1/collections/childrens', {
-          method: 'GET',
-          headers: {
-            'Authorization': 'Bearer ' + accessTokenInput.value
-          }
-    })
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-
-      childCollections = data.items;
-
-      function findChildren(parentId, items) {
-        const children = items.filter(item => item.parent && item.parent.$id === parentId);
-
-        // Create an array to store the children of the current parent
-        const childrenArray = [];
-        
-        // Recursively call the function for each found child
-        children.forEach(child => {
-          let tmp = {
-            id: child._id,
-            title: child.title,
-            children: []
-          };
-        
-          // Recursively find and add children to the current child
-          tmp.children = findChildren(tmp.id, items);
-
-          // Add the current child to the array
-          childrenArray.push(tmp);
-         
-        });
-
-        return childrenArray;
-      }
-  
-     // console.log(collectionsInterface.chosenCollection);
-
-      selectedItems = findChildren(collectionsInterface.chosenCollection,childCollections);
-
-
-      populateBookmarks(selectedItems);
-      
-    }); //End then
+      syncInterface.grabBookmarksAndPopulate();
+    });
   });
-});
 
 //TODO:
 //- Folders with large amounts of items it is missing some items. In a folder of 83 items it is missing 8 (the Want folder) and in To Download (505 items) it only has about 275
-//Update status of what app is currently doing
+//- 
